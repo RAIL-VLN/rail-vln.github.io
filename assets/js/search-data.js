aa60724dd353e10c4a7e1e4cd341ed6f13f3c6c2
RavenKiller <41775391+RavenKiller@users.noreply.github.com>
2025-01-10 12:18:49 +0000

    Deploying to gh-pages from @ RAIL-VLN/rail-vln.github.io@4b6ac67521db46b738f6b20ce4b0039afe3bcb5f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -509,13 +509,6 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
